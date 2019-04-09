2ec62fa422552f1867ed16be4f6bb37c3613e5e6
sushil sundaram <sushil.sundaram@gmail.com>
2019-04-08 22:31:01 -0400

    modified code
    - added new map
    - created function to convert latitude and longitude

--- make_dataset.py
+++ make_dataset.py
@@ -17,6 +17,17 @@ data_dict = {
     "abs_up_acc": 0
     }
 
+def parse_dms(lat, latdir, lon, londir):
+    deg = int(lat/100)
+    seconds = lat - (deg * 100)
+    latdec  = deg + (seconds/60)
+    if latdir == 'S': latdec = latdec * -1
+    deg = int(lon/100)
+    seconds = lon - (deg * 100)
+    londec  = deg + (seconds/60)
+    if londir == 'W': londec = londec * -1
+    return latdec, londec
+
 # with open('accel.csv') as f:
 #     reader = csv.reader(f)
 #     for row in reader:
@@ -48,9 +59,10 @@ with open('data.txt', 'r') as f:
         time = temp[0].split(':')
         # print(temp)
         # print(time)
+        lat, lon = parse_dms(float(temp[1]), temp[2], float(temp[3]), temp[4])
         data_dict['timestamp'] = float(time[0])*3600 + float(time[1])*60 + float(time[2])
-        data_dict['gps_lat'] = float(temp[1])*0.01
-        data_dict['gps_lon'] = float(temp[3])*(-0.01)
+        data_dict['gps_lat'] = lat
+        data_dict['gps_lon'] = lon
         data_dict['abs_north_acc'] = float(temp[7])
         data_dict['abs_east_acc'] = float(temp[6])
         data_dict['abs_up_acc'] = 0
--- plot.py
+++ plot.py
@@ -26,36 +26,24 @@ gmap1 = gmplot.GoogleMapPlotter(predicted_latitude_list[0],
 
 # scatter method of map object  
 # scatter points on the google map 
-# gmap1.scatter( latitude_list, longitude_list, '# FF0000', 
-                            #   size = 0.5, marker = False ) 
-gmap1.scatter( predicted_latitude_list, predicted_longitude_list, '# 0000FF', 
-                              size = 0.5, marker = False )
+gmap1.scatter( predicted_latitude_list, predicted_longitude_list, '#FF0000', 
+                              size = 0.1, marker = False )
 
 # Plot method Draw a line in 
 # between given coordinates 
-gmap1.plot(predicted_latitude_list, predicted_longitude_list,  
-           'cornflowerblue', edge_width = 2.5) 
+# gmap1.plot(predicted_latitude_list, predicted_longitude_list,  
+#            'cornflowerblue', edge_width = 1.5) 
   
-# Pass the absolute path 
-gmap1.draw( "/home/sushils/Documents/kalman/kalman-filter/predicted_map_tanu_new.html" )
-
-# GoogleMapPlotter return Map object 
-# Pass the center latitude and 
-# center longitude 
-gmap2 = gmplot.GoogleMapPlotter(latitude_list[0],
-                                longitude_list[0], 13 ) 
 
 # scatter method of map object  
 # scatter points on the google map 
-# gmap1.scatter( latitude_list, longitude_list, '# FF0000', 
-                            #   size = 0.5, marker = False ) 
-gmap2.scatter( latitude_list, longitude_list, '# 0000FF', 
-                              size = 0.5, marker = False )
+gmap1.scatter( latitude_list, longitude_list, '#0000FF', 
+                              size = 0.15, marker = False )
 
 # Plot method Draw a line in 
 # between given coordinates 
-gmap2.plot(latitude_list, longitude_list,  
-           'cornflowerblue', edge_width = 2.5) 
+# gmap2.plot(latitude_list, longitude_list,  
+#            'cornflowerblue', edge_width = 1.5) 
   
 # Pass the absolute path 
-gmap2.draw( "/home/sushils/Documents/kalman/kalman-filter/actual_map_tanu_new.html" )
+gmap1.draw( "/home/sushils/Documents/kalman/kalman-filter/map_tanu_new.html" )
